d9c0e410ddd2776b4ceeb7275bc075a626f20219
YeongShu Chun <118553927+yanggank1212@users.noreply.github.com>
2023-11-08 20:45:06 -0330

    Login page view
    basic login page view yet,

--- poutine_time/lib/main.dart
+++ poutine_time/lib/main.dart
@@ -1,18 +1,96 @@
 import 'package:flutter/material.dart';
-import 'package:poutine_time/views/feed_page.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: FeedpageWidget(),
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+/* step 1 Creta themain layout of the app
+
+
+
+ */
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: LoginScreen());
+  }
+}
+
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: const [
+          Text(
+              "Poutine Time",
+              style:TextStyle(
+                color: Colors.black,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+              ),
+          ),
+          Text("Login to your account", style: TextStyle(color: Colors.black,
+          fontSize: 44.0,
+          fontWeight: FontWeight.bold
+          ),
+          ),
+          SizedBox(
+            height: 44.0,
+          ),
+          TextField(
+            keyboardType: TextInputType.emailAddress,
+            decoration: InputDecoration(
+            hintText: "Uesr Email",
+            prefixIcon: Icon(Icons.mail, color:Colors.brown),
+            ),
+          ),
+          SizedBox(
+            height: 26.0,
+          ),
+          TextField(
+            obscureText: true,
+            decoration: InputDecoration(
+             hintText: "User Password",
+             prefixIcon: Icon(Icons.lock, color:Colors.brown),
+          ),
+          ),
+          Text(
+              "Sign in?",
+               style: TextStyle(color: Colors.blue),
+          )
+      ]),
     );
   }
 }
