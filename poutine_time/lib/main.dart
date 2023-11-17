32c327c083ab89d34cbb979490fba37a8ad157db
Prosper Manyele <77206118+Mank1941@users.noreply.github.com>
2023-11-17 02:16:59 -0330

    Firebase Authentication Set Up

--- poutine_time/lib/main.dart
+++ poutine_time/lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:poutine_time/firebase_options.dart';
 import 'package:poutine_time/views/home_page.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'views/auth_gate.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MainApp());
 }
 
